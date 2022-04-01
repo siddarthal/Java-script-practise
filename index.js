b9b5fb734ceef16503eec7063a253b1942dc71c4
Siddartha <sidhureddy.legala@gmail.com>
2022-04-01 17:13:45 +0530

    hi

---
+++ index.js
@@ -0,0 +1,11 @@
+console.time('mycode');
+console.log('hi');
+
+// console.log(4+8);
+console.log([1,2,2,3]);
+
+console.table({sidha : 'this',age:19});
+console.warn('hi ');
+
+console.timeEnd('mycode');
+console.assert(age>119,'age >119 is not psoobile');
