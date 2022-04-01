b263fddc640dac34cf43b2c635c6aa288dfcbb67
Siddartha <sidhureddy.legala@gmail.com>
2022-04-01 19:44:15 +0530

    sggood

---
+++ tut3.js
@@ -0,0 +1,31 @@
+console.log('hi');
+// variables in js
+// var,let,const
+var names ='sidha';
+
+var channel;
+channel='code with sidha';
+var marks=34;
+console.log(names ,channel,marks);
+const named='hai sid';
+console.log(named);
+var city='wgl';
+console.log(city)
+{
+    let city='ram'
+    city='kolkat';
+    console.log(city);
+}
+console.log(city);
+
+const arr1 =[21,13,23,34];
+arr1.push(45);
+console.log(arr1);
+/*most programming case types
+
+1.camelCase
+2.kebab-case
+3.snake_case
+4.PascalCase
+
+*/
---
+++ tut4.js
@@ -0,0 +1,59 @@
+// string
+let names = 'sodhi';
+console.log(names);
+console.log('my name is ' +names);
+console.log('my name is ' +(typeof names));
+
+// Numbers
+
+let marks = 34;
+console.log('my name is ' +(typeof marks));
+// boolean
+
+let isDriver=true;
+console.log('my name is ' +(typeof isDriver));
+
+// null
+
+let nullVar =null;
+console.log('my name is ' +(typeof nullVar));
+
+// undefined
+
+let unDef = undefined;
+console.log('my name is ' +(typeof
+    undefined));
+
+    // Reference
+
+    myArr =[1,1,1,1,1,'fals',false];
+    console.log('my name is ' +(typeof myArr));
+
+    // objects literals
+
+    let marksa ={
+        sidha:342,
+        sidhaw:344,
+        sidha2ed:345,
+
+    }
+    console.log(typeof marksa);
+
+    // functions]
+
+    function findName() {
+        
+        
+    }
+ console.log(typeof findName);
+
+ let date =new Date()
+ console.log(typeof date);
+
+
+
+
+
+
+
+
